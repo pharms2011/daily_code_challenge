e066ab01ae502d7adf45ed3d3fab835eea68d3d4
Preston Harms <prestonharms5@gmail.com>
2021-12-14 14:40:22 -0800

    12-14-21
    Python bool to str

---
+++ daily_code_challenge_python/daily_code_challenge/string/boolean_to_string.py
@@ -0,0 +1,12 @@
+def boolean_to_string(b):
+    return "True" if(b)  else "False";
+
+# import codewars_test as test
+# from solution import boolean_to_string
+#
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(boolean_to_string(True), "True")
+#         test.assert_equals(boolean_to_string(False), "False")
